e5745a99562964859fd89d8787288e801ddf3d00
mukthar <muktharmanaf28@gmail.com>
2025-01-21 15:45:26 +0530

    server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hallo world')
+import { Client, GatewayIntentBits } from 'discord.js';
+import * as dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+
+
+
+// Event listeners
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+// Log in
+client.login(process.env.DISCORD_TOKEN);
